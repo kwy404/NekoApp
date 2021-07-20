17c6d2b562af8bba32676d473f9a5848e0df56e2
Gustavo Melo <gmelo6546@gmail.com>
2021-07-19 22:21:08 -0300

    refactor and add internacionalization

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -3,19 +3,12 @@ require 'digest'
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def generate_token
-    string_length = 120
-    # Generate a token for the user
-    return rand(36**string_length).to_s(36)
-  end
-
   def update(id)
     @user = User.find(id)
-    @user.token = generateToken()
+    @user.token = generate_token
     @user.save
   end
 
-  # Reset password by id
   def reset_password
     @user = User.find(params[:id])
     @user.password = params[:password]
@@ -23,12 +16,14 @@ class Api::V1::UsersController < ApplicationController
     @user.save
   end
 
-  def validationToken
+  def validation_token
     @user = User.find_by(token: params[:token])
     if @user
       render json: @user, status: :ok
     else
       render json: { error: 'Token inválido' }, status: :unprocessable_entity
+    end
+  end
 
   def login
     password_encrypted = Digest::SHA2.hexdigest params[:password]
@@ -38,24 +33,23 @@ class Api::V1::UsersController < ApplicationController
       update(@user._id)
       render json: @user, status: :ok
     else 
-      render json: {status: "Error", message: "E-mail ou senha Inválidos."}, status: :unauthorized
+      render json: {
+        status: "Error",
+        message: I18n.t("api.v1.users.login.invalid_data.message")
+      }, status: :unauthorized
     end
   end
 
   def create
-    if email_already_created()
-      render json: {status: "Error", message: "E-mail já cadastrado."}, status: :unauthorized
-      return
-    end
-    if username_already_created()
-      render json: {message: "Nome de usuário já cadastrado"}, status: :unprocessable_entity
-      return
-    end
+    return email_already_create_response if email_already_created?
+
+    return username_already_create_response if username_already_created?
+
     password_encrypted = Digest::SHA2.hexdigest params[:password]
     @user = User.create(username: params[:username], 
                         email: params[:email], 
                         password: password_encrypted,
-                        token: generateToken(),
+                        token: generate_token,
                         admin: 'False',
                         beta: :beta,
                         avatar: "",
@@ -68,23 +62,54 @@ class Api::V1::UsersController < ApplicationController
     if @user.save
       render json: @user, status: :ok
     else
-      render json: {message: "usuário não criado"}, status: :unprocessable_entity
+      generic_error_message()
     end
   end
 
   private
-    def email_already_created
+
+    def generate_token
+      string_length = 120
+
+      return rand(36**string_length).to_s(36)
+    end
+
+    def email_already_created?
       @validation = User.find_by({ "email": params[:email] })
       if @validation
         true
       end
     end
 
-    def username_already_created
+    def username_already_created?
       @validation = User.find_by({ "username": params[:username] })
       if @validation
         true
       end
     end
 
+    def update(id)
+    @user = User.find(id)
+    @user.token = generate_token
+    @user.save
+  end
+
+  def email_already_create_response
+    render json: {
+        status: "Error",
+        message: I18n.t("api.v1.users.register.email_already_exist.message")
+      }, status: :unprocessable_entity
+  end
+
+  def username_already_create_response
+    render json: {
+      message: I18n.t("api,v1.users.register.username_already_exist")
+    }, status: :unprocessable_entity
+  end
+
+  def generic_error_message
+    render json: {
+      message: I18n.t("api.v1.users.register.error")
+    }, status: :unprocessable_entity
+  end
 end
